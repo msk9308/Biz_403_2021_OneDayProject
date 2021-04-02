f76ab56d109c7976721f3efe8ba6cdc4b72abfc0
msk9308 <msk9308@naver.com>
2021-04-02 09:23:54 +0900

    OneDay

---
+++ Java_OneDay10_Score/src/com/callor/score/service/ScoreServiceV1.java
@@ -0,0 +1,22 @@
+package com.callor.score.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScoreServiceV1 {
+
+	protected Scanner scan;
+	protected List<ScoreVO> numList;
+
+	public ScoreServiceV1() {
+
+		scan = new Scanner(System.in);
+
+	}
+
+	public void inputNum() {
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+#One Day Project
+
+## Java 성적처리
+* 2021-04-02
+* Java_oneDay10_Score
+* Java_Console 성적처리 프로젝트
